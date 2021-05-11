81bdf47c4c28b43b01fcac0d8de1beb860915e26
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-05-11 13:12:15 +0100

    added location error display

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
+const err = document.querySelector(".error");
+const map = document.querySelector("#map");
+const refresh = document.querySelector(".refresh img");
+
+function successLocation(position) {
+  setMap([position.coords.longitude, position.coords.latitude]);
+}
+function errorLocation() {
+  err.classList.add("show");
+  map.classList.add("hide");
+}
+
+function setMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15,
+  });
+}
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccurcy: true,
+});
+
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYm9zc29uY29kZSIsImEiOiJja29qeHRoZHAwZHo2MnBxZndjanYycGU3In0.r_T8-h5iPsxR87Dq5nbHqQ";
-var map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
+refresh.addEventListener("click", (e) => {
+  location.reload();
 });
