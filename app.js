b63acc1fae83652ab4919dbdfa69aba68020b4d5
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-05-11 12:21:17 +0100

    initialized git repo and added map

---
+++ app.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYm9zc29uY29kZSIsImEiOiJja29qeHRoZHAwZHo2MnBxZndjanYycGU3In0.r_T8-h5iPsxR87Dq5nbHqQ";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+});
